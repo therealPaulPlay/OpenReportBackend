6643533692fab04466af173a7d6ac63c318eb376
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2024-12-31 19:07:33 +0100

    email change

--- JS/sendEmails.js
+++ JS/sendEmails.js
@@ -4,6 +4,7 @@ const nodemailer = require('nodemailer');
 let transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
