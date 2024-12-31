7260d31c75476d4ba302827d84874ab3f9f90689
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2024-12-31 19:12:36 +0100

    test fix

--- JS/sendEmails.js
+++ JS/sendEmails.js
@@ -3,8 +3,7 @@ const nodemailer = require('nodemailer');
 // Configure your email service
 let transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: true,
+    port: Number(process.env.EMAIL_PORT),
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
